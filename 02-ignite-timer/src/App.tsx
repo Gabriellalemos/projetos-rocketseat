f97b91ebf61762816b70f2136a498ce7ac160ac3
colaborador <colaborador@N091153.local>
2023-02-19 14:39:00 -0300

    add projeto ignite-timer

---
+++ 02-ignite-timer/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import Router from "./Router";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
+
+export default App;
